f7e89c2d859882dbf5170108ff0e76b513bd325d
Patrick Kilgore <pckilgore@noreply.github.com>
2018-06-11 13:48:38 -0500

    initial commit

---
+++ mergesort.spec.js
@@ -0,0 +1,17 @@
+describe('Merge Sort', function() {
+  it('handles an empty array', function() {
+    expect(mergeSort([])).toEqual([]);
+  });
+  it('handles a singleton', function() {
+    expect(mergeSort([1])).toEqual([1]);
+  });
+  it('handles a sorted array', function() {
+    expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('handles an reversed array', function() {
+    expect(mergeSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+  });
+  it('handles a random array', function() {
+    expect(mergeSort([2, 1, 3, 5, 4])).toEqual([1, 2, 3, 4, 5]);
+  });
+});
